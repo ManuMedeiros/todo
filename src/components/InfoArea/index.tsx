a03320c43c1197e4987347b63ff3eba28411dd1d
ManuMedeiros <manu.medeiros94@gmail.com>
2022-08-16 10:38:59 -0300

    new main

--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -1,48 +1,46 @@
-import * as S from './styles';
-import { formatCurrentMonth } from '../../helpers/dataFilter';
-import ResumeItem from '../ResumeItem'
+import * as C from './styles';
+import { formatCurrentMonth } from '../../helpers/dateFilter';
+import { ResumeItem } from '../ResumeItem';
 
 type Props = {
     currentMonth: string;
-    onMonthChange: (newMOnth: string) => void;
+    onMonthChange: (newMonth: string) => void;
     income: number;
     expense: number;
 }
 
-const InfoArea = ({ currentMonth, onMonthChange, income, expense }: Props) => {
+export const InfoArea = ({ currentMonth, onMonthChange, income, expense }: Props) => {
+    
     const handlePrevMonth = () => {
         let [year, month] = currentMonth.split('-');
-        let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1)
-        currentDate.setMonth(currentDate.getMonth() - 1)
-        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`)
+        let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
+        currentDate.setMonth( currentDate.getMonth() - 1 );
+        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
     }
 
     const handleNextMonth = () => {
         let [year, month] = currentMonth.split('-');
-        let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1)
-        currentDate.setMonth(currentDate.getMonth() + 1)
-        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`)
+        let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
+        currentDate.setMonth( currentDate.getMonth() + 1 );
+        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
     }
 
-
     return (
-        <S.Container>
-            <S.MonthArea>
-                <S.MonthArrow onClick={handlePrevMonth}>⬅️</S.MonthArrow>
-                <S.MonthTitle>{formatCurrentMonth(currentMonth)}</S.MonthTitle>
-                <S.MonthArrow onClick={handleNextMonth}>➡️</S.MonthArrow>
-            </S.MonthArea>
-            <S.ResumeArea>
+        <C.Container>
+            <C.MonthArea>
+                <C.MonthArrow onClick={handlePrevMonth}>⬅️</C.MonthArrow>
+                <C.MonthTitle>{formatCurrentMonth(currentMonth)}</C.MonthTitle>
+                <C.MonthArrow onClick={handleNextMonth}>➡️</C.MonthArrow>
+            </C.MonthArea>
+            <C.ResumeArea>
                 <ResumeItem title="Receitas" value={income} />
                 <ResumeItem title="Despesas" value={expense} />
                 <ResumeItem
                     title="Balanço"
                     value={income - expense}
-                    color={income - expense < 0 ? 'red' : 'green'}
+                    color={(income-expense) < 0 ? 'red' : 'green'}
                 />
-            </S.ResumeArea>
-        </S.Container>
+            </C.ResumeArea>
+        </C.Container>
     );
 }
-
-export default InfoArea;
--- src/components/InfoArea/styles.ts
+++ src/components/InfoArea/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    background-color: #fff;
-    box-shadow: 0 0 5px #ccc;
+    background-color: #FFF;
+    box-shadow: 0px 0px 5px #CCC;
     border-radius: 10px;
     padding: 20px;
     margin-top: -40px;
